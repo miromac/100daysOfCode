6fd8ec077ef96407a32b9e072b6dbed014bd78de
Miro Mac <itcodevelopment@gmail.com>
2021-12-21 12:14:07 +0200

    The First Day - The Beginning of Course

---
+++ day-1/main.py
@@ -0,0 +1,22 @@
+# Day 1 - String manipulation
+
+# Printing new lines with backslash and n "\n"
+print("Hello world!\nHello world!\nHello world!")
+# String concatenation with the "+" sign
+print("Hello" + " " +"world!")
+# input() function and input("A prompt for the user")
+#input("What is your name? ")
+# Prints "Hello" and user's input prompt and "!""
+#print("Hello " + input("What is your name? ") + "!")
+# The length of string
+#print(len(input("What is your name? ")))
+# Variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+# Values stored in the variables a and b
+a = input("a:")
+b = input("b:")
+print("a = " + a)
+print("b = " + b)
---
+++ day-1/name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the name generator!\n")
+city = input("Which city did you grow up in?\n")
+pet = input("WHat is the name of a pet?\n")
+print("Your name could be " + city + " " + pet)
